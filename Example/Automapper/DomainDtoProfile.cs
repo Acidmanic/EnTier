72c61dae1a5f4cc8f5ff63ebed2290057a0e8723
Mani Moayedi <you@example.com>
2020-03-19 13:08:44 +0330

    Fix Example project

--- Example/Automapper/DomainDtoProfile.cs
+++ Example/Automapper/DomainDtoProfile.cs
@@ -10,10 +10,6 @@ public class DomainDtoProfile:Profile{
 
     public DomainDtoProfile()
     {
-        CreateMap<LoginResponse,LoginResponseDto>()
-            .ReverseMap()
-        ;
-
         CreateMap<DomainModels.User,DataTransferModels.User>()
             .ReverseMap()
         ;
