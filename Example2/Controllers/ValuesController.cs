46a984e2e9733195e974d8c2ace6d37c5cd299e2
Mani Moayedi <you@example.com>
2020-03-19 13:11:49 +0330

    Ex2: Add controller

--- Example2/Controllers/ValuesController.cs
+++ Example2/Controllers/ValuesController.cs
@@ -2,44 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Models;
+using Plugging;
 
 namespace Example2.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class UsersController : EnTierControllerBase<UserInfo,UserInfo,UserInfo,long>
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        public UsersController(IObjectMapper mapper) : base(mapper)
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
