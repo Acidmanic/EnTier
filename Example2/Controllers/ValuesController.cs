279fa1d999ee44321f019bd25a9e62c0b97a0cfb
Mani Moayedi <you@example.com>
2020-03-19 12:23:10 +0330

    Add New Scratch Example

--- Example2/Controllers/ValuesController.cs
+++ Example2/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace mock_server.Controllers
+namespace Example2.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
