5d14ceb4d82a2e79f9748ae6e03a063f4f0adfb3
Mani Moayedi <you@example.com>
2020-03-19 13:08:07 +0330

    Add Model

---
+++ Example2/Models/UserInfo.cs
@@ -0,0 +1,31 @@
+
+
+
+using System;
+
+namespace Models{
+
+
+
+
+    public class UserInfo{
+
+
+        public string Name{get;set;}
+
+        public string Surname {get;set;}
+
+        public string IdCardSerial{get;set;}
+
+        public long Id{get;set;}
+
+        public DateTime BirthDate{get;set;}
+
+        public DateTime RegistrationDate {get;set;}
+
+        public string Biography {get;set;}
+
+
+        
+    }
+}
