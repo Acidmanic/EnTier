d5e0692621c6283386fa6a99414a242eb6049857
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2020-03-29 00:11:07 +0430

    Split FileContext

--- EnTier/Context/FileContext.cs
+++ EnTier/Context/FileContext.cs
@@ -1,83 +1,13 @@
-﻿using Components;
-using Newtonsoft.Json;
-using Plugging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace Context
 {
-    public class FileContext : IContext, IEnTierBuiltIn
+    public class FileContext:FileContextBase,IEnTierBuiltIn
     {
-
-        private class JsonIdGenerator : IIDGenerator
+        public FileContext():base()
         {
-
-            private JsonDatabase _db;
-
-            public JsonIdGenerator(JsonDatabase db)
-            {
-                _db = db;
-            }
-            public long NewId<T>()
-            {
-                return _db.GenerateId<T>();
-            }
-        }
-
-
-
-        private string dbDirectory;
-
-        private object _filesLock = new object();
-
-        private JsonDatabase _database = new JsonDatabase();
-
-        public FileContext()
-        {
-
-            dbDirectory = Path.Combine(Environment.CurrentDirectory, "SerializedDatabase");
-
-            if (!Directory.Exists(dbDirectory))
-            {
-                Directory.CreateDirectory(dbDirectory);
-            }
-
-
-            Load();
-
-        }
-
-        private void Load()
-        {
-            lock (_filesLock)
-            {
-                _database.Load(dbDirectory);
-            }
-        }
-
-  
-
-        public virtual void Apply()
-        {
-            lock (_filesLock)
-            {
-                _database.Save(dbDirectory);
-            }
-        }
-
-        public virtual void Dispose()
-        {
-            // Disposed ofcourse!
-        }
-
-        public IDataset<T> GetDataset<T>() where T : class
-        {
-            var datalist = _database.Table<T>();
-
-            return new JsonDataset<T>(datalist, new JsonIdGenerator(_database));
 
         }
     }
---
+++ EnTier/Context/FileContextBase.cs
@@ -0,0 +1,88 @@
+﻿using Components;
+using Newtonsoft.Json;
+using Plugging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Context
+{
+    public abstract class FileContextBase : IContext
+    {
+
+        private class JsonIdGenerator : IIDGenerator
+        {
+
+            private JsonDatabase _db;
+
+            public JsonIdGenerator(JsonDatabase db)
+            {
+                _db = db;
+            }
+            public long NewId<T>()
+            {
+                return _db.GenerateId<T>();
+            }
+        }
+
+        private string dbDirectory;
+
+        private object _filesLock = new object();
+
+        private JsonDatabase _database = new JsonDatabase();
+
+
+        public FileContextBase(string dbDirectory)
+        {
+            Load(dbDirectory);
+        }
+
+        public FileContextBase()
+        {
+
+            var dbDirectory = Path.Combine(Environment.CurrentDirectory, "SerializedDatabase");
+
+            Load(dbDirectory);
+
+        }
+
+        private void Load(string dbDirectory)
+        {
+            lock (_filesLock)
+            {
+
+                if (!Directory.Exists(dbDirectory))
+                {
+                    Directory.CreateDirectory(dbDirectory);
+                }
+
+                _database.Load(dbDirectory);
+            }
+        }
+
+  
+
+        public virtual void Apply()
+        {
+            lock (_filesLock)
+            {
+                _database.Save(dbDirectory);
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            // Disposed ofcourse!
+        }
+
+        public IDataset<T> GetDataset<T>() where T : class
+        {
+            var datalist = _database.Table<T>();
+
+            return new JsonDataset<T>(datalist, new JsonIdGenerator(_database));
+
+        }
+    }
+}
