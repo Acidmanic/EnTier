3fcb423d9080cdd996c5c4cf5a91288a1d5c768a
Mani Moayedi <you@example.com>
2021-05-21 10:15:23 +0430

    Add Inmemory/SignleLayerEntity example

---
+++ ExampleInMemorySingleLayerEntity/Controllers/PostsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnTier.Controllers;
+using ExampleInMemorySingleLayerEntity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ExampleInMemorySingleLayerEntity.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostsController : CrudControllerBase<Post,string>
+    {
+        
+    }
+}
