5723b94545f4240cd55a5f278726957e722bd28b
Mani Moayedi <acidmanic.moayedi@gmail.com>
2022-03-08 16:23:14 +0330

    Update EntityFrameworkUnitOfwork to be able to instanciate CrudRepositorties with any dbset in their constructors

--- EnTier.DataAccess.EntityFramework/EntityFrameworkUnitOfWork.cs
+++ EnTier.DataAccess.EntityFramework/EntityFrameworkUnitOfWork.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using EnTier.Repositories;
 using EnTier.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
@@ -8,10 +10,24 @@ namespace EnTier.DataAccess.EntityFramework
     public class EntityFrameworkUnitOfWork : IUnitOfWork
     {
         private readonly DbContext _context;
-
+        private readonly Func<Type, object> GetDbSetByType;
         public EntityFrameworkUnitOfWork(DbContext context)
         {
             _context = context;
+
+
+            var contextType = typeof(DbContext);
+
+            var getDbSetMethod = contextType.GetMethod(nameof(DbContext.Set));
+
+            GetDbSetByType = entityType =>
+            {
+                var method = getDbSetMethod?.MakeGenericMethod(new Type[] { });
+
+                return method?.Invoke(_context, new object[] { });
+            };
+
+
         }
 
 
@@ -22,24 +38,108 @@ namespace EnTier.DataAccess.EntityFramework
             return new EntityFrameWorkCrudRepository<TStorage, TId>(dbSet);
         }
 
-    
+
         public TCustomCrudRepository GetCrudRepository<TStorage, TId, TCustomCrudRepository>()
             where TStorage : class, new()
-            where TCustomCrudRepository:ICrudRepository<TStorage,TId>
+            where TCustomCrudRepository : ICrudRepository<TStorage, TId>
         {
-            var dbSet = _context.Set<TStorage>();
-
             var repoType = UnitOfWorkRepositoryConfigurations.GetInstance().GetRepositoryType<TCustomCrudRepository>();
 
             if (repoType == null)
             {
-                throw new Exception("You should register your custom repository in your startup class, using applicationBuilder.");
+                throw new Exception(
+                    "You should register your custom repository in your startup class, using applicationBuilder.");
             }
 
-            var repository  = repoType.GetConstructor(new Type[] {typeof(DbSet<TStorage>)})
-                .Invoke(new object[]{dbSet});
-            
+            var repoConstructor = GetConstructor(repoType);
+
+            if (repoConstructor == null)
+            {
+                throw new Exception("Your repository type should have" +
+                                    " a constructor with (0 to any number " +
+                                    "of) DbSet<TDalModel> arguments.");
+            }
+
+            object[] arguments = ProvideDbSetArgs(repoConstructor);
+
+            var repository = repoConstructor.Invoke(arguments);
+
             return (TCustomCrudRepository) repository;
+        }
+
+        private object[] ProvideDbSetArgs(ConstructorInfo repoConstructor)
+        {
+            var dbSetTypes = repoConstructor.GetParameters()
+                .Select( p => p.ParameterType).ToList();
+
+            var parameterValues = new object[dbSetTypes.Count];
+
+            for (int i = 0; i < parameterValues.Length; i++)
+            {
+                var modelTypes = dbSetTypes[i].GenericTypeArguments;
+
+                if (modelTypes == null || modelTypes.Length != 1)
+                {
+                    throw  new Exception(dbSetTypes[i].FullName +
+                                         " is not acceptable for a CrudRepository " +
+                                         "constructor argument.");
+                }
+
+                object dbSet = GetDbSetByType(modelTypes[0]);
+
+                parameterValues[i] = dbSet;
+            }
+
+            return parameterValues;
+        }
+
+        private ConstructorInfo GetConstructor(Type repoType)
+        {
+            var constructors = repoType.GetConstructors();
+
+            var dbSetOnlyConstructors = constructors.Where(c => IsDbSetOnly(c.GetParameters()));
+
+            var setOnlyConstructors = dbSetOnlyConstructors as ConstructorInfo[] ?? dbSetOnlyConstructors.ToArray();
+
+            if (setOnlyConstructors.Any())
+            {
+                var theConstructor = setOnlyConstructors[0];
+
+                int longest = theConstructor.GetParameters().Length;
+
+                foreach (var c in setOnlyConstructors)
+                {
+                    var len = c.GetParameters().Length;
+
+                    if (len > longest)
+                    {
+                        longest = len;
+
+                        theConstructor = c;
+                    }
+                }
+
+                return theConstructor;
+            }
+
+            return null;
+        }
+
+        private bool IsDbSetOnly(ParameterInfo[] parameters)
+        {
+            var dbSetType = typeof(DbSet<>);
+            
+            foreach (var parameter in parameters)
+            {
+                var type = parameter.ParameterType;
+
+                if (!type.IsInstanceOfType(dbSetType))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public void Complete()
