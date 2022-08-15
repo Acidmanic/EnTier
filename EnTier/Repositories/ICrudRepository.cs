b8197fcbfe460d7c3893acd66677f2fe24a3f8df
Mani Moayedi <acidmanic.moayedi@gmail.com>
2022-08-15 14:27:01 +0430

    Update ICrudRepository to be able to be used for seeding purposes

--- EnTier/Repositories/ICrudRepository.cs
+++ EnTier/Repositories/ICrudRepository.cs
@@ -5,21 +5,48 @@ using System.Linq.Expressions;
 
 namespace EnTier.Repositories
 {
-    public interface ICrudRepository<TStorage,TId>
-    where TStorage:class,new()
+    public interface ICrudRepository<TStorage, TId>
+        where TStorage : class, new()
     {
-
-
+        /// <summary>
+        /// Provides all existing instances of Entity
+        /// </summary>
         IEnumerable<TStorage> All();
 
+        /// <summary>
+        /// Adds new instance of Entity
+        /// </summary>
         TStorage Add(TStorage value);
-        
+
+        /// <summary>
+        /// Updates an existing instance of entity for its properties
+        /// </summary>
+        TStorage Update(TStorage value);
+
+        /// <summary>
+        /// If the passed value, is an existing Entity, then it's properties,
+        /// will be updated, Otherwise, the new value will be added. 
+        /// </summary>
+        TStorage Set(TStorage value);
+
+        /// <summary>
+        /// Finds an existing entity by its Id
+        /// </summary>
         TStorage GetById(TId id);
 
-        IEnumerable<TStorage> Find(Expression<Func<TStorage,bool>> predicate);
+        /// <summary>
+        /// Performs a search inside the existing entities.
+        /// </summary>
+        IEnumerable<TStorage> Find(Expression<Func<TStorage, bool>> predicate);
 
+        /// <summary>
+        /// Removes an existing Entity
+        /// </summary>
         bool Remove(TStorage value);
 
+        /// <summary>
+        /// Removes an existing Entity
+        /// </summary>
         bool Remove(TId id);
     }
 }
