03a5dc6640ac8b9c04f56af7c188051a965851a1
Mani Moayedi <acidmanic.moayedi@gmail.com>
2022-08-16 13:29:10 +0430

    Test/Fix InMemory and JsonFil examples

--- Example.JsonFile/Domain/Post.cs
+++ Example.JsonFile/Domain/Post.cs
@@ -1,9 +1,12 @@
 using System;
+using Acidmanic.Utilities.Reflection.Attributes;
 
 namespace ExampleJsonFile.Domain
 {
     public class Post
     {
+        [AutoValuedMember]
+        [UniqueMember]
         public string Id { get; set; }
         
         public string Title { get; set; }
--- Example.JsonFile/Storage/PostStg.cs
+++ Example.JsonFile/Storage/PostStg.cs
@@ -1,9 +1,12 @@
 using System;
+using Acidmanic.Utilities.Reflection.Attributes;
 
 namespace ExampleJsonFile.Storage
 {
     public class PostStg
     {
+        [AutoValuedMember]
+        [UniqueMember]
         public string Id { get; set; }
         
         public string Title { get; set; }
