e64dc2bbc91f66a428b28525212d6dc76f9bbab9
Mani Moayedi <acidmanic.moayedi.com>
2022-10-05 09:09:06 +0330

    Add Async methods to IRepository

--- EnTier/Repositories/ICrudRepository.cs
+++ EnTier/Repositories/ICrudRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace EnTier.Repositories
 {
@@ -48,5 +49,48 @@ namespace EnTier.Repositories
         /// Removes an existing Entity
         /// </summary>
         bool Remove(TId id);
+        
+        //>>>>>>>>>>>>>>>>
+        
+        /// <summary>
+        /// Provides all existing instances of Entity
+        /// </summary>
+        Task<IEnumerable<TStorage>> AllAsyn();
+
+        /// <summary>
+        /// Adds new instance of Entity
+        /// </summary>
+        Task<TStorage> AddAsync(TStorage value);
+
+        /// <summary>
+        /// Updates an existing instance of entity for its properties
+        /// </summary>
+        Task<TStorage> UpdateAsync(TStorage value);
+
+        /// <summary>
+        /// If the passed value, is an existing Entity, then it's properties,
+        /// will be updated, Otherwise, the new value will be added. 
+        /// </summary>
+        Task<TStorage> SetAsync(TStorage value);
+
+        /// <summary>
+        /// Finds an existing entity by its Id
+        /// </summary>
+        Task<TStorage> GetByIdAsync(TId id);
+
+        /// <summary>
+        /// Performs a search inside the existing entities.
+        /// </summary>
+        Task<IEnumerable<TStorage>> FindAsync(Expression<Func<TStorage, bool>> predicate);
+
+        /// <summary>
+        /// Removes an existing Entity
+        /// </summary>
+        Task<bool> RemoveAsync(TStorage value);
+
+        /// <summary>
+        /// Removes an existing Entity
+        /// </summary>
+        Task<bool> RemoveAsync(TId id);
     }
 }
