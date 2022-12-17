5f7e327c56d995d364386e28a5459c0be95f8cb2
Mani Moayedi <acidmanic.moayedi.com>
2022-12-17 22:44:59 +0330

    Add ApplicationBuilder Extensions also to ServiceProvider'

--- EnTier/Extensions/ServiceProviderExtensions.cs
+++ EnTier/Extensions/ServiceProviderExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using EnTier.DependencyInjection;
+using EnTier.Exceptions;
 using EnTier.Fixture;
 
 namespace EnTier.Extensions
@@ -12,6 +14,27 @@ namespace EnTier.Extensions
                 FixtureManager.UseFixture<TFixture>(essence);    
             }
             
+            return serviceProvider;
+        }
+        
+        public static EnTierEssence GetRegisteredEnTierEssence(this IServiceProvider serviceProvider)
+        {
+            var essence = serviceProvider.GetService(typeof(EnTierEssence)) as EnTierEssence;
+
+            if (essence == null)
+            {
+                throw new EnTierEssenceIsNotRegisteredException();
+            }
+
+            return essence;
+        }
+        
+        public static IServiceProvider ConfigureEnTierResolver(this IServiceProvider serviceProvider)
+        {
+            var essence = serviceProvider.GetRegisteredEnTierEssence();
+
+            essence.UseResolver(new DotnetResolverFacade(serviceProvider));
+
             return serviceProvider;
         }
     }
