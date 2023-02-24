416ba913aee82b3c293a669689d2e08a43a7a08a
Mani Moayedi <acidmanic.moayedi.com>
2023-02-25 00:28:12 +0330

    Implement Builtin EventStreamRepositories

--- EnTier/DataAccess/InMemory/InMemoryEventStreamRepository.cs
+++ EnTier/DataAccess/InMemory/InMemoryEventStreamRepository.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Acidmanic.Utilities.Reflection;
-using Acidmanic.Utilities.Reflection.Extensions;
 using EnTier.Repositories;
 using EnTier.Repositories.Models;
 using EnTier.Utility;
@@ -41,56 +39,18 @@ public class InMemoryEventStreamRepository<TEvent, TEventId, TStreamId>
              .Where(se => streamId.Equals(se.StreamId)));
      }
 
-     private bool IsGreaterThan(TEventId greater, TEventId smaller)
-     {
-         if (greater == null)
-         {
-             return false;
-         }
-
-         if (smaller == null)
-         {
-             return true;
-         }
-
-         var type = greater.GetType();
-         
-         if (TypeCheck.IsReferenceType(type))
-         {
-             return false;
-         }
-
-         if (greater is string sGreater && smaller is string sSmaller)
-         {
-             return string.CompareOrdinal(sGreater, sSmaller) > 0;
-         }
-         
-         if (greater is char cGreater && smaller is char cSmaller)
-         {
-             return cGreater > cSmaller;
-         }
-
-         if (TypeCheck.IsNumerical(type))
-         {
-             var gNumber = greater.AsNumber();
-             var sNumber = smaller.AsNumber();
-
-             return gNumber > sNumber;
-         }
-
-         return false;
-     }
+     
      
      protected override Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>> ReadEntryChunk(TStreamId streamId, TEventId baseEventId, long count)
      {
           return Task.FromResult(Entries
              .Where(se => streamId.Equals(se.StreamId))
-             .Where(se => IsGreaterThan(se.EventId , baseEventId)));
+             .Where(se => ObjectOperations.IsGreaterThan(se.EventId , baseEventId)));
      }
 
      protected override Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>> ReadEntryChunk(TEventId baseEventId, long count)
      {
          return Task.FromResult(Entries
-             .Where(se => IsGreaterThan(se.EventId , baseEventId)));
+             .Where(se => ObjectOperations.IsGreaterThan(se.EventId , baseEventId)));
      }
 }
--- EnTier/DataAccess/JsonFile/JsonFileUnitOfWork.cs
+++ EnTier/DataAccess/JsonFile/JsonFileUnitOfWork.cs
@@ -191,6 +191,11 @@ namespace EnTier.DataAccess.JsonFile
             }
         }
 
+        public override IEventStreamRepository<TEvent, TEventId, TStreamId> GetStreamRepository<TEvent, TEventId, TStreamId>()
+        {
+            return new JsonFileEventStreamRepository<TEvent, TEventId, TStreamId>();
+        }
+
         protected override ICrudRepository<TStorage, TId> CreateDefaultCrudRepository<TStorage, TId>()
         {
             var table = Table<TStorage>();
