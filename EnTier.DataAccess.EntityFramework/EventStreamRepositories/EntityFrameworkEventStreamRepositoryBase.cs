2e754ee9bd38f4d09f046ef57d87b8a0cba20431
Mani Moayedi <acidmanic.moayedi.com>
2023-03-05 10:45:58 +0330

    fix forgotten constructor inheritance of essence parameter in ef-es-repo base

--- EnTier.DataAccess.EntityFramework/EventStreamRepositories/EntityFrameworkEventStreamRepositoryBase.cs
+++ EnTier.DataAccess.EntityFramework/EventStreamRepositories/EntityFrameworkEventStreamRepositoryBase.cs
@@ -9,16 +9,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EnTier.DataAccess.EntityFramework.EventStreamRepositories;
 
-internal abstract class EntityFrameworkEventStreamRepositoryBase<TEvent,TEventId,TStreamId>:EventStreamRepositoryBase<TEvent,TEventId,TStreamId>
+internal abstract class
+    EntityFrameworkEventStreamRepositoryBase<TEvent, TEventId, TStreamId> : EventStreamRepositoryBase<TEvent, TEventId,
+        TStreamId>
 {
-    public EntityFrameworkEventStreamRepositoryBase(DbSet<EfObjectEntry<TEventId, TStreamId>> dbSet,EnTierEssence essence)
+    public EntityFrameworkEventStreamRepositoryBase(DbSet<EfObjectEntry<TEventId, TStreamId>> dbSet,
+        EnTierEssence essence) : base(essence)
     {
         DbSet = dbSet;
     }
 
     protected DbSet<EfObjectEntry<TEventId, TStreamId>> DbSet { get; }
-    
-    
+
+
     protected override TEventId AppendEntry(ObjectEntry<TEventId, TStreamId> entry)
     {
         var addedEntry = DbSet.Add(entry.ToEf());
@@ -38,14 +41,15 @@ internal abstract class EntityFrameworkEventStreamRepositoryBase<TEvent,TEventId
         return result;
     }
 
-   
+
     protected abstract Expression<Func<EfObjectEntry<TEventId, TStreamId>, bool>>
         StreamIdEqualityExpression(TStreamId streamId);
-    
+
     protected abstract Expression<Func<EfObjectEntry<TEventId, TStreamId>, bool>>
-        EventIdLargerThanExpression(TEventId baseEventId); 
-    
-    protected override async Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>> ReadEntriesByStreamId(TStreamId streamId)
+        EventIdLargerThanExpression(TEventId baseEventId);
+
+    protected override async Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>> ReadEntriesByStreamId(
+        TStreamId streamId)
     {
         var entries = await DbSet
             .Where(StreamIdEqualityExpression(streamId))
@@ -54,7 +58,7 @@ internal abstract class EntityFrameworkEventStreamRepositoryBase<TEvent,TEventId
         return entries;
     }
 
-    protected override async Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>> 
+    protected override async Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>>
         ReadEntryChunk(TStreamId streamId, TEventId baseEventId, long count)
     {
         var entries = await DbSet
@@ -66,7 +70,8 @@ internal abstract class EntityFrameworkEventStreamRepositoryBase<TEvent,TEventId
         return entries;
     }
 
-    protected override async Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>> ReadEntryChunk(TEventId baseEventId, long count)
+    protected override async Task<IEnumerable<ObjectEntry<TEventId, TStreamId>>> ReadEntryChunk(TEventId baseEventId,
+        long count)
     {
         var entries = await DbSet
             .Where(EventIdLargerThanExpression(baseEventId))
