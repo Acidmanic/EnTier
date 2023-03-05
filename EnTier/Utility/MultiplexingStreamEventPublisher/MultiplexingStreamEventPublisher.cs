62c45d8763fb3b2d30c4e1e21246230134f15b3d
Mani Moayedi <acidmanic.moayedi.com>
2023-03-05 13:12:53 +0330

    Make MultiplexingStreamEventPublisher able to resolve and add publishers by type

--- EnTier/Utility/MultiplexingStreamEventPublisher/MultiplexingStreamEventPublisher.cs
+++ EnTier/Utility/MultiplexingStreamEventPublisher/MultiplexingStreamEventPublisher.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using EnTier.EventSourcing;
+using Microsoft.Extensions.Logging;
 
 namespace EnTier.Utility.MultiplexingStreamEventPublisher;
+
 /// <summary>
 /// This class helps multiplexing publication of events from a single source. Since this implements the
 /// <code>IStreamPublisherAdapter</code> interface, it can be registered in Di and be automatically detected by
@@ -15,7 +17,14 @@ public class MultiplexingStreamEventPublisher : IStreamEventPublisherAdapter
     private readonly List<Func<Action<object, object, object>>> _publisherFactories =
         new List<Func<Action<object, object, object>>>();
 
-    private readonly object _publishLock = new object(); 
+    private readonly object _publishLock = new object();
+
+    private readonly EnTierEssence _essence;
+
+    public MultiplexingStreamEventPublisher(EnTierEssence essence)
+    {
+        _essence = essence;
+    }
 
     public void Publish(object @event, object eventId, object streamId)
     {
@@ -45,11 +54,37 @@ public class MultiplexingStreamEventPublisher : IStreamEventPublisherAdapter
 
     public void Add(Func<IStreamEventPublisherAdapter> publisherFactory)
     {
-        Add(() => (e,i,s) => publisherFactory().Publish(e,i,s));
+        Add(() => (e, i, s) => publisherFactory().Publish(e, i, s));
     }
-    
+
     public void Add(IStreamEventPublisherAdapter adapter)
     {
         Add(() => adapter.Publish);
+    }
+
+    public void Add<TPublisher>()
+        where TPublisher : IStreamEventPublisherAdapter
+    {
+        Add(typeof(TPublisher));
+    }
+
+    public void Add(Type publisherType)
+    {
+        Func<Action<object, object, object>> factory = () =>
+        {
+            var publisher = _essence.ResolveOrDefault(publisherType, () => null);
+
+            if (publisher is IStreamEventPublisherAdapter adapter)
+            {
+                return adapter.Publish;
+            }
+
+            return (o, o1, arg3) =>
+            {
+                _essence.Logger.LogError("MultiplexingStreamEventPublisher was not able to instantiate " +
+                                         "{TypeName}. Please make sure this class is registered in your di",
+                    publisherType.FullName);
+            };
+        };
     }
 }
